111b216a005314c29a46c1d0517604ae5288f3a3
Kevin Bacino <kevin.bacino@gmail.com>
2017-07-01 19:03:46 -0400

    added additional methods

--- Java 8 Testing/src/com/testing/Sorting.java
+++ Java 8 Testing/src/com/testing/Sorting.java
@@ -3,6 +3,7 @@ package com.testing;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Sorting {
 
@@ -15,6 +16,28 @@ public class Sorting {
 				  .thenComparing(p -> p.getLastName()));
 		
 		personList.forEach(p -> System.out.println(p));
+		
+		List<Person> personList2 = personList.stream()
+				                             .filter(x -> x.getAge() > 22)
+				                             .sorted(Comparator.comparing(p -> ((Person) p).getLastName()).reversed())
+				                             .collect(Collectors.toList());
+		System.out.println();
+		personList2.forEach(x -> System.out.println(x));
+		
+		Person pMinAge = personList.stream()
+								   .min((p1, p2) -> Integer.compare(p1.getAge(), p2.getAge())).get();
+		
+		System.out.println();
+		System.out.println(pMinAge);
+		
+		Person person = personList.stream()
+								  .filter(x -> x.getAge() > 22)
+								  .sorted(Comparator.comparing(x -> -1*x.getAge()))
+								  .findFirst()
+								  .orElse(null);
+		
+		System.out.println();
+		System.out.println(person);
 		
 		
 
